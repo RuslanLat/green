e93c63d647fcea4158deb6e8825fa08d0c245606
Ruslan Latipov <rus_kadr03@mail.ru>
2023-05-20 21:43:14 +0300

    add files

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import sqlite3
 import pandas as pd
-import numpy as np
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(page_title="Плант Бокс - технологии возделывания лекарственных",
-                   page_icon="/images/favicon.ico")
+                   page_icon="/images/favicon.png",
+                   layout="wide")
 
 custom_css = """<style>
                     .championship-tab-company {
@@ -26,6 +28,7 @@ st.markdown("""<div class="championship-tab-company-container">
                         alt="partner">
                 </div>""", unsafe_allow_html=True)
 
+st.write("##")
 
 st.write(""" В настоящее время ощущаются перебои в поставках различных экстрактов и лекарственных субстанций. По статистике по разным позициям импорт составляет от 20 до 80% всего используемого сырья. Важнейшая задача – помочь российским фермерам вырастить сырье на их земле.  
 
@@ -33,66 +36,49 @@ st.write(""" В настоящее время ощущаются перебои 
 
 Данное решение будет способствовать устранению проблемы нехватки консолидированной информации по возделыванию лекарственных культур. """)
 
+
 # подключение к базе данных (в случае отсутствия база данных создается)
 connection = sqlite3.connect("data/plantarium.db")
 # создание объекта подключения
 cursor = connection.cursor()
 
-cursor.execute("""SELECT plant_ru_name
-                  FROM plant_ru;""")
-
-plant_ru_names = cursor.fetchall()
-plant_ru_names = [plant_ru_name[0] for plant_ru_name in plant_ru_names]
-
 cursor.execute("""SELECT region_name
-                  FROM regions;""")
-
+                    FROM regions;""")
 region_names = cursor.fetchall()
 region_names = [region_name[0] for region_name in region_names]
 
-st.markdown("""
-    <style>
-        .stMultiSelect [data-baseweb=label] span{
-            max-width: 400px;
-            font-size: 0.6rem;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+st.subheader("Введите (выбирете) наименование региона")
 
-with st.sidebar:
-    st.header("Выбор параметров поиска")
-    region_options = st.multiselect(
-                        label = 'Наименование региона',
-                        options=region_names,
-                        default=region_names[:3],
-                        help="Выбирете (введите) наименование региона",
-                        ) #label_visibility="hidden"
-    plant_ru_options = st.multiselect(
-                        label = 'Наименование растения',
-                        options=plant_ru_names,
-                        default=plant_ru_names[:3],
-                        help="Выбирете (введите) наименование растения",
-                        ) #label_visibility="hidden"
+select_option = st.selectbox('', region_names,
+                            help="Введите (выбирете) наименование региона", index=12)
 
-cursor.execute("""SELECT region_name, plant_ru_name, plant_lat_name, book_name, book_year, book_web
-                  FROM regions
-                  INNER JOIN books USING(region_id)
-                  INNER JOIN redbooks USING(book_id)
-                  INNER JOIN plant_ru USING(plant_ru_id)
-                  INNER JOIN plant_lat USING(plant_lat_id);""")
+cursor.execute("""SELECT plant_ru_name, plant_lat_name, book_name, book_year, book_web
+                    FROM regions
+                    INNER JOIN books USING(region_id)
+                    INNER JOIN redbooks USING(book_id)
+                    INNER JOIN plant_ru USING(plant_ru_id)
+                    INNER JOIN plant_lat USING(plant_lat_id)
+                    WHERE region_name == ?;""", (select_option,))
 
 plants_data = cursor.fetchall()
-colnames = ["Наименование региона", "Название растения RU",
-            "Название растения LAT", "Наименование книги (источник)",
-           "Год книги", "Веб сайт книги"]
-books_df = pd.DataFrame(data=plants_data, columns=colnames) #, columns=colnames
-#books_df["Год книги"] = books_df["Год книги"].fillna("-  ").astype("str").apply(lambda x: x[:-2])
+colnames = ["Название растения RU",
+                "Название растения LAT", "Наименование книги (источник)",
+                "Год книги", "Веб сайт книги"]
+books_df = pd.DataFrame(data=plants_data, columns=colnames)
 
-st.write("Список растений в красных книгах")
+st.subheader("Список растений в красных книгах")
 
-st.write(books_df[:20])
-
-connection.close()
+gd = GridOptionsBuilder.from_dataframe(books_df)
+gd.configure_pagination(enabled=True,
+                            paginationAutoPageSize=False,
+                            paginationPageSize=15)
+gd.configure_default_column(editable=True, groupable=True)
+    #gd.configure_selection(selection_mode="multiple", use_checkbox=True)
+gridoptions = gd.build()
+grid_table = AgGrid(books_df, gridOptions=gridoptions,
+                        update_mode=GridUpdateMode.SELECTION_CHANGED,
+                        allow_unsafe_jscode=True,
+                        theme="alpine")
 
 st.write("##")
 st.markdown("<h5 style='text-align: center; color: blac;'> ©️ Команда Extreme DS </h5>", unsafe_allow_html=True)
